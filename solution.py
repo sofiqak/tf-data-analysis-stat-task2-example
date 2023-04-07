01c19e58b793ecb5a9eda8fa8d0f588505e92442
sofiqak <130180826+sofiqak@users.noreply.github.com>
2023-04-07 23:13:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,14 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id =  917079889 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    mean = np.mean(x)
+    variance = np.var(x)
+    df = len(x) - 1
+    critical = norm.ppf((1 + p) / 2, df)
+    lower_bound = (df * variance) / critical
+    upper_bound = (df * variance) / norm.ppf((1 - p) / 2, df)
+    confidence_interval = (lower_bound, upper_bound)
+    return confidence_interval
