e4e1b7c5f9a04242cb880c4f230ddce11b84a1ae
sofiqak <130180826+sofiqak@users.noreply.github.com>
2023-04-08 00:16:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as norm
 
-from scipy.stats import norm
-
-
-chat_id =  917079889 # Ваш chat ID, не меняйте название переменной
+chat_id = 917079889 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    mean = np.mean(x)
-    variance = np.var(x)
-    df = len(x) - 1
-    critical = norm.ppf((1 + p) / 2, df)
-    lower_bound = (df * variance) / critical
-    upper_bound = (df * variance) / norm.ppf((1 - p) / 2, df)
-    return lower_bound, \
-           upper_bound
+    n = len(x)
+    x_bar = np.mean(x)
+    s = np.std(x)
+    chi_sq_left = norm.chi2.ppf((1 - p/2), df=n-1)
+    chi_sq_right = norm.chi2.ppf(p/2, df=n-1)
+    left = np.sqrt((n-1)*s**2/chi_sq_left)
+    right = np.sqrt((n-1)*s**2/chi_sq_right)
+    return (x_bar - left, x_bar + right)
